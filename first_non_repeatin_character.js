4ff87e15bbaab991e1a2d813a074de1c81783407
ANOOP <connect.anoopjaiswal@gmail.com>
2024-11-26 07:14:03 +0530

    first non repeating character in a string

--- first_non_repeatin_character.js
+++ first_non_repeatin_character.js
@@ -1,26 +1,3 @@
-### Occurrence of character in string
-
-```javascript
-const occurence = (char) => {
-  const obj = {};
-
-  for (let i = 0; i < char.length; i++) {
-    if (!obj[char[i]]) {
-      obj[char[i]] = 1;
-    } else {
-      obj[char[i]]++;
-    }
-  }
-
-  return obj;
-};
-
-console.log(occurence("javascript")); //{ j: 1, a: 2, v: 1, s: 1, c: 1, r: 1, i: 1, p: 1, t: 1 }
-console.log(occurence("jaavaa")); //{ j: 1, a: 4, v: 1 }
-
-### Occurrence of character in string
-
-
 const firstNonReapeatingChar = (char) => {
   const obj = {};
 
