d938ed235567ccb7210ca79699967546e8c64951
ANOOP <connect.anoopjaiswal@gmail.com>
2025-08-31 16:29:39 +0530

    valid paranthesi

---
+++ validParanthesis.js
@@ -0,0 +1,23 @@
+const isValid = (str) => {
+  const stack = [];
+
+  for (let char of str) {
+    if (char === "(" || char === "{" || char === "[") {
+      stack.push(char);
+    } else {
+      const last = stack.pop();
+
+      if (
+        (char === "]" && last !== "[") ||
+        (char === ")" && last !== "(") ||
+        (char === "}" && last !== "{")
+      ) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
+
+console.log(isValid("{[]}"));
